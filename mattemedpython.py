cfeae14cbaa24697e74c6e7d0f4d964138754d62
henrik_o <henrik.ostensen@gmail.com>
2021-08-24 13:29:14 +0200

    matte med trekant

--- mattemedpython.py
+++ mattemedpython.py
@@ -1,6 +1,12 @@
-h=int(input('Hva er høyden?: '))
-b=int(input('Hva er bredden?: '))
+#firkant
+h=int(input('Hva er høyden på firkanten?: '))
+b=int(input('Hva er bredden på firkanten?: '))
 a = h * b
 o = 2 * h + 2 * b
-print('Arealet er '+ str(a))
-print('Omkretsen er '+ str(o))
+print('Arealet til firkanten er: '+ str(a))
+print('Omkretsen til firkanten er: '+ str(o))
+#trekant
+h2=int(input('Hva er høyden på trekanten?: '))
+g=int(input('Hva er grunnlinjen på trekanten?: '))
+a2 = h2 * g / 2
+print('Arealet til trekanten er: '+str(a2))
